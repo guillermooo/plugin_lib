f20bb4d4ad8790361e2b71f8419fee1d150953e7
guillermooo <guillermo.lopez@outlook.com>
2015-10-19 00:13:55 +0200

    remove line

--- collections.py
+++ collections.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2014, Guillermo López-Anglada. Please see the AUTHORS file for details.
+﻿# Copyright (c) 2014, Guillermo López-Anglada. Please see the AUTHORS file for details.
 # All rights reserved. Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.)
 
@@ -8,7 +8,6 @@ from collections import deque
 class CircularArray(object):
     def __init__(self, initial_data, *args, **kwargs):
         self.data = deque(initial_data)
-        self.index = None
 
     def forward(self):
         item = self.data.popleft()
