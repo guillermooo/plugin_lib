5c98ae06cd4294c9d29eb99031ec3ca1433900d8
guillermooo <guillermo.lopez@outlook.com>
2015-10-18 12:52:31 +0200

    refactor

--- collections.py
+++ collections.py
@@ -2,32 +2,23 @@
 # All rights reserved. Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.)
 
+from collections import deque
 
-class CircularArray(list):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+
+class CircularArray(object):
+    def __init__(self, initial_data, *args, **kwargs):
+        self.data = deque(initial_data)
         self.index = None
 
     def forward(self):
-        if self.index is None:
-            self.index = 0
-            return self[self.index]
-
-        try:
-            self.index += 1
-            return self[self.index]
-        except IndexError:
-            self.index = 0
-            return self[self.index]
+        item = self.data.popleft()
+        self.data.append(item)
+        return item
 
     def backward(self):
-        if self.index is None:
-            self.index = -1
-            return self[self.index]
+        item = self.data.pop()
+        self.data.appendleft(item)
+        return item
 
-        try:
-            self.index -= 1
-            return self[self.index]
-        except IndexError:
-            self.index = -1
-            return self[self.index]
+    def __len__(self):
+        return len(self.data)
